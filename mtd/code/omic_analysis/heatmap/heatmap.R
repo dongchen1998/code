f6eb0c1b98f53bdf3832dfa29adc854895479064
dongchen <1219131962@qq.com>
2024-01-08 09:50:25 +0800

    main

--- mtd/code/omic_analysis/heatmap/heatmap.R
+++ mtd/code/omic_analysis/heatmap/heatmap.R
@@ -3,7 +3,7 @@ library(pheatmap)
 library(dplyr)
 library(ggplot2)
 
-# Rscript heatmap.R --input input_file/expression_matrix_heatmap2.csv --output output_file/heatmap.png --color_up "#F76809" --color_down "#0766AD" --color_mid "#FFFFFF" --show_border TRUE --scale row --cluster_rows FALSE --cluster_cols FALSE --cellwidth 20 --cellheight 20 --fontsize 10
+# Rscript heatmap.R --input input_file/expression_matrix_heatmap.csv --output output_file/heatmap.png --color_up "#F76809" --color_down "#0766AD" --color_mid "#FFFFFF" --show_border TRUE --scale row --cluster_rows TRUE --cluster_cols FALSE --cellwidth 20 --cellheight 20 --fontsize 10
 
 
 # 定义命令行选项
@@ -14,9 +14,9 @@ option_list <- list(
     make_option(c("--color_down"), type = "character", default = "#0766AD", help = "低表达量颜色"),
     make_option(c("--color_mid"), type = "character", default = "#FFFFFF", help = "中等表达量颜色"),
     make_option(c("--show_border"), type = "logical", default = FALSE, help = "是否显示边框"),
-    make_option(c("--scale"), type = "character", default = "row", help = "标准化方式"),
     make_option(c("--cluster_rows"), type = "logical", default = FALSE, help = "是否对基因进行聚类"),
     make_option(c("--cluster_cols"), type = "logical", default = FALSE, help = "是否对样本进行聚类"),
+    make_option(c("--scale"), type = "character", default = "row", help = "标准化方式"),
     make_option(c("--cellwidth"), type = "numeric", default = 20, help = "单元格宽度"),
     make_option(c("--cellheight"), type = "numeric", default = 20, help = "单元格高度"),
     make_option(c("--fontsize"), type = "numeric", default = 10, help = "字体大小")
